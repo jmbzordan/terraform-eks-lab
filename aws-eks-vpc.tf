70b98adcf8daae0f0e5fb5f0ab8fd507e8923550
joao_zordan <joao_zordan@sicredi.com.br>
2024-10-13 17:59:19 -0300

    Ajustes

--- aws-eks-vpc.tf
+++ aws-eks-vpc.tf
@@ -8,40 +8,31 @@ resource "aws_vpc" "vpc" {
 }
 
 
-# Cria dinamicamente um mapa de subnets por availability zone conforme especificado pelo usuario nas variaveis
-# Divide igualmente a quantidade de subnets entre publica e privada ---> Possível problema de a quantidade não for multipla de 2 e também não multipla 
-# do número de availability Zones. Porém foi descartado pela dificuldade de toda vez iterar sobre um grande mapa e testando se publica ou privada.
-# FICA O APRENDIZADO
-/*
-resource "aws_subnet" "subnet_list" {
-  for_each = local.subnets
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = cidrsubnet(var.cidr_block, 8, each.value.subnet_num + index(var.availability_zones, each.value.az) * 2) 
-    availability_zone       = each.value.az
-    map_public_ip_on_launch = ((((each.value.subnet_num + 1) % 2) == 0) ? false : true)             # Se true a subnet é publica, ou seja, quando um recurso cair nessa subnet, automaticamente ele ganha um IP público. Senão é privado
-    tags                    = { Name = "eks-subnet-${each.value.az}-${each.value.subnet_num}" }
-}
-*/
-
 resource "aws_subnet" "public_subnets" {
   for_each                  = { for key,value in local.subnets : key => value if value.subnet_num == 0 }
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = cidrsubnet( var.cidr_block, 8, each.value.subnet_num + index(var.availability_zones, each.value.az) * 2 ) 
-    availability_zone       = each.value.az
-    map_public_ip_on_launch = true
-    tags                    = { Name = "${var.project_name}-subnet-${each.value.az}-pub-${each.value.subnet_num}",
-                                "kubernetes.io/role/elb" = 1 }
+     vpc_id                  = aws_vpc.vpc.id
+     cidr_block              = cidrsubnet( var.cidr_block, 8, (each.value.subnet_num * 2) + index(var.availability_zones, each.value.az) ) 
+     availability_zone       = each.value.az
+     map_public_ip_on_launch = true
+     tags                    = { 
+                                Name = "${var.project_name}-subnet-${each.value.az}-pub-${each.value.subnet_num + 1}",
+                                "kubernetes.io/role/elb" = 1 
+                              }
 }
 
+
 resource "aws_subnet" "private_subnets" {
-  for_each                  = { for key,value in local.subnets : key => value if value.subnet_num == 1 }
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = cidrsubnet( var.cidr_block, 8, each.value.subnet_num + index(var.availability_zones, each.value.az) * 2 ) 
-    availability_zone       = each.value.az
-    map_public_ip_on_launch = false
-    tags                    = { Name = "${var.project_name}-subnet-${each.value.az}-pub-${each.value.subnet_num}",
-                                "kubernetes.io/role/internal-elb" = 1 }
+  for_each                  = { for key,value in local.subnets : key => value if value.subnet_num != 0 }
+     vpc_id                  = aws_vpc.vpc.id
+     cidr_block              = cidrsubnet( var.cidr_block, 8, (each.value.subnet_num * 2) + index(var.availability_zones, each.value.az) ) 
+     availability_zone       = each.value.az
+     map_public_ip_on_launch = false
+     tags                    = { 
+                                Name = "${var.project_name}-subnet-${each.value.az}-priv-${each.value.subnet_num + 1}",
+                                "kubernetes.io/role/internal-elb" = 1 
+                              }
 }
+
 
 # Cria internet gateway que será associada as subnets publicas nas route tables, para rotas externas
 # É criado somente um para toda a VPC
@@ -52,57 +43,45 @@ resource "aws_internet_gateway" "igateway" {
 
 
 # Toda nat_gateway necessita se atrelada a um elastic IP. Logo, cria-se tantos quantos for o número de nat_gateways (CARAI TA CERTA ESSA FRASE?)
-resource "aws_eip" "elastic_ip" {
+resource "aws_eip" "elastic_ips" {
   for_each = { for idx,az in var.availability_zones : idx => az }
-    domain = "vpc"
-    tags   = { Name = "${var.project_name}-elastic-ip-${each.value}" }
+     domain = "vpc"
+     tags   = { Name = "${var.project_name}-elastic-ip-${each.value}" }
 }
 
-/*
+
 # Nat gateway são utilizadas pelas subnets privadas para chegar ao internet gateway e ter acesso externo. É criada uma para cada availability zone
 # sendo utilizada como rota para todas as subnets daquela availability zone. OBS: São criadas nas subnets publicas!!
 resource "aws_nat_gateway" "ngateway" {
-  count = length(var.availability_zones)
-    allocation_id = aws_eip.elastic_ip[count.index].allocation_id
-    subnet_id     = values(local.public_subnets)[count.index].id
-    tags          = { Name = "eks-nat-gateway-${count.index}" }
+  for_each        = { for idx,az in var.availability_zones : idx => az }
+     allocation_id = aws_eip.elastic_ips[each.key].allocation_id
+     subnet_id     = values(aws_subnet.public_subnets)[each.key].id 
+     tags          = { Name = "${var.project_name}-nat-gateway-${each.value}",
+                       "AZ" = "${each.value}" }
 }
-*/
 
-#Criação de routetable de forma dinamica. Funciona, mas acredito ser melhor declarar independentemente com nomes public e private
-# devido a possivel necessidade de outros resources iterar apenas com as publicas ou apenas com as privadas e não com toda a lista
-/* FICA O APRENDIZADO
-resource "aws_route_table" "routetable" {
-  count = 2
-  vpc_id = aws_vpc.vpc.id
-  dynamic "route" {
-    for_each = (count.index == 0 ? [1] : [])
-    content {  
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.gateway.id
-    }
-  }
-}
-*/
 
 # Route Table publica da VPC, associada ao cidr 0.v.0.0/0 que é o da internet
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id       = aws_vpc.vpc.id
   route { 
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igateway.id
   }
-  tags   = { Name = "${var.project_name}-public-routetable" }
+  tags         = { Name = "${var.project_name}-public-routetable" }
 }
 
 
 # Route Table privada da VPC, ASSOCIA-SE AS NAT_GATEWAYS??? NAO SEI AINDA
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
-#  route {
-#    nat_gateway_id = aws_nat_gateway.ngateway.id
-#  }
-  tags   = { Name = "${var.project_name}-private-routetable" }
+  for_each            = aws_nat_gateway.ngateway
+     vpc_id           = aws_vpc.vpc.id
+     route {
+       cidr_block     = "0.0.0.0/0"
+       nat_gateway_id = "${aws_nat_gateway.ngateway[each.key].id}"
+     }
+     tags             = { Name = "${var.project_name}-private-routetable-${each.value.tags["AZ"]}" 
+                          "AZ" = "${each.value.tags["AZ"]}" }
 }
 
 
@@ -110,23 +89,26 @@ resource "aws_route_table" "private_route_table" {
  # # Possibilidade de iterar diretamente sem o locals, porém menos elegante.
  # for_each = { for key,value in aws_subnet.subnet_list : key => value if value.map_public_ip_on_launch }
 resource "aws_route_table_association" "public_rta" {
-  for_each         = aws_subnet.public_subnets
-    route_table_id = aws_route_table.public_route_table.id
-    subnet_id      = "${each.value.id}"
+  for_each          = aws_subnet.public_subnets
+     route_table_id = aws_route_table.public_route_table.id
+     subnet_id      = "${each.value.id}"
 }
 
 
 # Cria dinamicamente as rtas privadas conforme mapa de subnets quem não possuem o parametro map_public_on_launch ativado
  # # Possibilidade de iterar diretamente sem o locals, porém menos elegante
+
  # for_each = { for key,value in aws_subnet.subnet_list : key => value if !value.map_public_ip_on_launch }
 resource "aws_route_table_association" "private_rta" {
-  for_each         = aws_subnet.private_subnets
-    route_table_id = aws_route_table.private_route_table.id
-    subnet_id      = "${each.value.id}"
+  for_each         = local.rt_subnet_az
+    route_table_id = each.value
+    subnet_id      = each.key
 }
 
+/*
 resource "aws_security_group" "security_group" {
   vpc_id = aws_vpc.vpc.id
   name   = "eks-securitygroup"
   tags   = { Name = "${var.project_name}-security-group" }
 }
+*/
--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,50 @@
-/*# Debug para printar a lista de subnets criadas
-output "subnet_list" {
-  description = "Imprime a lista de subnets criadas para a VPC do cluster k8s"
-  value = {for key,value in aws_subnet.subnet_list : key => value}
+output "private_subnets" {
+  value = aws_subnet.private_subnets
+}
+
+output "rt_subnet_association" {
+  value = local.rt_subnet_az
+}
+/*
+output "nat_gateways" {
+  value = aws_nat_gateway.ngateway 
 }
 */
 /*
-output "route_tables" {
-  description = "Imprime a Route Table criada"
-  value = {for key,value in aws_route_table_association.public_rta : key => value}
+output "subnet_nats" {
+  value = local.matching_subnets
 }
 */
-# Imprime os IDs das subnets criadas
+##### OUTPUTS PARA DEBUG #####
+/*
+# Imprime as subnets PUBLICAS criadas
 output "public_subnets" {
    value = {for key,value in aws_subnet.public_subnets : key => value}
 }
-
+*/
+/*
+# Imprime as subnets PRIVADAS criadas
 output "private_subnets" {
    value = {for key,value in aws_subnet.private_subnets : key => value}
 }
+*/
 /*
-# debug de tentativa de criaçao dinamica de lista de subnets
+# Imprime o resultado dos 3 for aninhados do local.subnets
 output "subnets_list" {
   value = local.subnets
 }
 */
 
-
-/*
 output "routes" {
   description = "route tables"
-  value = [for i in aws_route_table.routetable : aws_route_table.routetable]
+  value = aws_route_table.private_route_table
+}
+
+
+/*# Debug para printar a lista dinamica de TODAS subnets criadas
+  # NÃO É MAIS UTILIZADO!!! Porém o resource dinamica está comentado em aws-eks-vpc.tf
+output "subnet_list" {
+  description = "Imprime a lista de subnets criadas para a VPC do cluster k8s"
+  value = {for key,value in aws_subnet.subnet_list : key => value}
 }
 */
