2948b6c7bbbaf81be98c99c78ea7fd507c2b2efd
jmbzordan <45662213+jmbzordan@users.noreply.github.com>
2024-10-17 13:33:14 -0300

    Comentários para documentação.

--- aws-eks-key-pair.tf
+++ aws-eks-key-pair.tf
@@ -1,3 +1,5 @@
+# Key Pair usado idealmente em primeiro momento quando se pensava em criar um k8s manualmente. Porém a ideia foi ajustada para um EKS.
+
 # Cria um key pair para ser usado no ASG
 resource "aws_key_pair" "key_pair" {
    key_name   = "k8s-key"
